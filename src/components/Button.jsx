dd9265cfa530a756b5c7acd63dac2c223b6063d6
jrfalcao <juniorfalcao.jc@gmail.com>
2020-02-02 15:45:01 -0300

    files app calculator

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Button.css'
+
+export default props => 
+    <button className={`
+        button 
+        ${props.operation ? 'operation' : ''}
+`}>{props.label}</button>
+

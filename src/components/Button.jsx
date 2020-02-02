c9dfaf659098b9cf175552cee3667757c30423af
jrfalcao <juniorfalcao.jc@gmail.com>
2020-02-02 19:10:44 -0300

    Gerando botões e display

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import './Button.css'
 
-export default props => 
-    <button className={`
-        button 
-        ${props.operation ? 'operation' : ''}
-`}>{props.label}</button>
+export default props =>
+    //let classes = 'button';
+    <button className={`button`}>{props.label}</button>
+
 
